e687677c37981dc54f659eb4c5c9bc4eb122e853
Daniel Allen <dan@aeternagroup.net>
2026-07-13 19:33:57 +0100

    Update to schema version v4.80.0

--- variables.tf
+++ variables.tf
@@ -31,9 +31,9 @@ EOT
     location               = string
     name                   = string
     config_file            = optional(string)
-    enable_health_check    = optional(bool) # Default: false
+    enable_health_check    = optional(bool)
     end_of_life_date       = optional(string)
-    exclude_from_latest    = optional(bool) # Default: false
+    exclude_from_latest    = optional(bool)
     package_file           = optional(string)
     tags                   = optional(map(string))
     manage_action = object({
@@ -46,11 +46,19 @@ EOT
       media_link                 = string
     })
     target_region = list(object({
-      exclude_from_latest    = optional(bool) # Default: false
+      exclude_from_latest    = optional(bool)
       name                   = string
       regional_replica_count = number
-      storage_account_type   = optional(string) # Default: "Standard_LRS"
+      storage_account_type   = optional(string)
     }))
   }))
+  validation {
+    condition = alltrue([
+      for k, v in var.gallery_application_versions : (
+        length(v.target_region) >= 1
+      )
+    ])
+    error_message = "Each target_region list must contain at least 1 items"
+  }
 }
 
